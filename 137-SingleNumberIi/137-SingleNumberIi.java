fd605fb4786077edfbc0411978f5915804684a77
Dharneesh R <dharneesh912@gmail.com>
2026-07-14 14:14:38 +0530

    Create 137-SingleNumberIi

---
+++ 137-SingleNumberIi/137-SingleNumberIi.java
@@ -0,0 +1,16 @@
+// Last updated: 7/14/2026, 2:14:40 PM
+class Solution {
+    public int singleNumber(int[] nums) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i:nums){
+            map.put(i,map.getOrDefault(i,0)+1);
+        }
+        for(int key:map.keySet()){
+            if(map.get(key)==1){
+                return key;
+            }
+        }
+        
+        return 0;
+    }
+}
