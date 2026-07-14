044c8cf6c5f4446bfc2165416946e55345637709
Dharneesh R <dharneesh912@gmail.com>
2026-07-14 14:11:33 +0530

    Create 1556-MakeTwoArraysEqualByReversingSubarrays

---
+++ 1556-MakeTwoArraysEqualByReversingSubarrays/1556-MakeTwoArraysEqualByReversingSubarrays.java
@@ -0,0 +1,14 @@
+// Last updated: 7/14/2026, 2:11:35 PM
+class Solution {
+    public boolean canBeEqual(int[] target, int[] arr) {
+        Arrays.sort(target);
+        Arrays.sort(arr);
+        for(int i=0;i<target.length;i++){
+            if(target[i]!=arr[i]){
+                return false;
+            }
+        }
+        return true;
+        
+    }
+}
