731b9081afd4f03c384cea26fc5e7bbb8aede3f9
Dharneesh R <dharneesh912@gmail.com>
2026-07-14 13:53:17 +0530

    Create 3606-MinimumElementAfterReplacementWithDigitSum

---
+++ 3606-MinimumElementAfterReplacementWithDigitSum/3606-MinimumElementAfterReplacementWithDigitSum.java
@@ -0,0 +1,17 @@
+// Last updated: 7/14/2026, 1:53:20 PM
+class Solution {
+    public int minElement(int[] nums) {
+        int arr[]=new int[nums.length];
+        for(int i=0;i<nums.length;i++){
+            int num=nums[i];
+            int sum=0;
+            while(num!=0){
+                sum=sum+num%10;
+                num=num/10;
+            }
+            arr[i]=sum;
+        }
+        Arrays.sort(arr);
+        return arr[0];
+    }
+}
