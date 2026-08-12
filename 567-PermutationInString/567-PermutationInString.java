4dff35966713b16ec1d970090c22ab3bc59841a4
Dharneesh R <dharneesh912@gmail.com>
2026-08-12 12:04:47 +0530

    Create 567-PermutationInString

---
+++ 567-PermutationInString/567-PermutationInString.java
@@ -0,0 +1,15 @@
+// Last updated: 8/12/2026, 12:04:57 PM
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        char[] ch1=s1.toCharArray();
+        Arrays.sort(ch1);
+        for(int i=0;i<=s2.length()-s1.length();i++){
+            char[] ch2=s2.substring(i,i+s1.length()).toCharArray();
+            Arrays.sort(ch2);
+            if(Arrays.equals(ch1,ch2)){
+                return true;
+            }
+        }
+        return false;
+    }
+}
